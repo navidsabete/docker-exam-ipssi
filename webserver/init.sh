ccc5c5235fe23c18e7065c38bbaaf6f46b88bc5b
Navid SABETE <nevid.dev@gmail.com>
2026-01-19 17:06:11 +0100

    dockerfile built

--- webserver/init.sh
+++ webserver/init.sh
@@ -1,1 +1,22 @@
 #!/bin/bash
+
+set -e  # Stop le script si une commande échoue
+
+echo "[INFO] Démarrage temporaire de MariaDB pour initialisation..."
+service mysql start
+
+echo "[INFO] Création de la base de données et de l'utilisateur..."
+mysql -u root <<EOF
+CREATE DATABASE IF NOT EXISTS ${DB_NAME};
+CREATE USER IF NOT EXISTS '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_PASSWORD}';
+GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'localhost';
+FLUSH PRIVILEGES;
+EOF
+
+echo "[INFO] Base de données prête !"
+
+echo "[INFO] Arrêt temporaire de MariaDB avant lancement via Supervisor..."
+service mysql stop
+
+echo "[INFO] Démarrage de Supervisor..."
+exec /usr/bin/supervisord -n
